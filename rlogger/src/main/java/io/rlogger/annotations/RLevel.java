6af52a701dc1a368c70326a8e6d3a24681e6702c
Rafael Cabral <rcabral@4ward.com.br>
2018-02-26 18:05:00 -0300

    Estruturação inicial do projeto

---
+++ rlogger/src/main/java/io/rlogger/annotations/RLevel.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package io.rlogger.annotations;
+
+/**
+ * @author rcabral
+ *
+ */
+public enum RLevel {
+	
+	INFO, 
+	
+	DEBUG;
+	
+	//TODO: Implementar demais níveis
+}
---
+++ rlogger/src/main/java/io/rlogger/annotations/RLogger.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package io.rlogger.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author rcabral
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RLogger {
+
+	public RLevel level();
+
+}
